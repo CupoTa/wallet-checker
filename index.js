9480c5e966d2a1d9479bcfcc2969bd86d3d920dd
Денис <denis-prosalov@yandex.ru>
2023-12-07 13:38:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,78 +29,77 @@ function startExpressServer() {
         console.error(`[Express Server Error]: ${data}`)
     })
 }
-startExpressServer()
 
-// async function startMenu(menu) {
-//     let startOver = true
-//     if (menu === undefined) {
-//         mode = await entryPoint()
-//     } else {
-//         startOver = false
-//     }
+async function startMenu(menu) {
+    let startOver = true
+    if (menu === undefined) {
+        mode = await entryPoint()
+    } else {
+        startOver = false
+    }
 
-//     switch ("web") {
-//         case "web":
-//             startExpressServer()
-//             break
-//         case "zksync":
-//             await zkSyncFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "starknet":
-//             await starknetFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "layerzero":
-//             await layerzeroFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "zora":
-//             await zoraFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "base":
-//             await baseFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "aptos":
-//             await aptosFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "linea":
-//             await lineaFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "scroll":
-//             await scrollFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "evm":
-//             let chain = await evmNetwork()
-//             await evmFetchDataAndPrintTable(chain).catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "balances":
-//             let network = await chooiceNetwork()
-//             await balancesFetchDataAndPrintTable(network).catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "filter":
-//             await filterFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//         case "nft":
-//             await nftFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-//             if (startOver) await startMenu()
-//             break
-//     }
-// }
+    switch (mode) {
+        case "web":
+            startExpressServer()
+            break
+        case "zksync":
+            await zkSyncFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "starknet":
+            await starknetFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "layerzero":
+            await layerzeroFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "zora":
+            await zoraFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "base":
+            await baseFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "aptos":
+            await aptosFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "linea":
+            await lineaFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "scroll":
+            await scrollFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "evm":
+            let chain = await evmNetwork()
+            await evmFetchDataAndPrintTable(chain).catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "balances":
+            let network = await chooiceNetwork()
+            await balancesFetchDataAndPrintTable(network).catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "filter":
+            await filterFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+        case "nft":
+            await nftFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
+            if (startOver) await startMenu()
+            break
+    }
+}
 
-// const args = process.argv.slice(2)
-// let mode = args[0]
+const args = process.argv.slice(2)
+let mode = args[0]
 
-// if (mode === 'web') {
-//     startExpressServer()
-// } else {
-//     await startMenu(mode)
-// }
+if (mode === 'web') {
+    startExpressServer()
+} else {
+    await startMenu(mode)
+}
